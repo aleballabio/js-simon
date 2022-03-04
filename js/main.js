0dbaa8b4aeae43650b9c29d478ef8e4ef510529c
aleballabio <aleballabio05@gmail.com>
2022-03-04 15:09:24 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*  
+
+1:  Generare 5 numeri causali 
+- usare il Math.random e math floor come funzione
+- Mettere i numeri dentro un arrayRandom con parseInt in un cicloFOR
+- Display su Html con H1 dei numeri random
+
+
+2: Numeri visibili per 30 secondi e allo scadere scompaiono.
+
+- Usare la funzione nuova che scompaiono dopo una volta ( non a inteverallo).
+
+- Timer 30000ms o * 1000 ai secondi come conversione
+
+- Print su Html con H1 al centro  dello schermo ( grandi) e "" su H1 dopo i 30 secondi.
+
+3: Allo  scadere del tempo ( dentro la funzione) ,compaiono i prompt con i numeri da
+    inserire in ordine 5 prompt.
+
+    - ciclo for
+    - prompt con parseInt in Array
+    
+    - condizione che se il numero è incluso nell' ArrayRandom:
+    aumenta il contatore
+        e lo pusha in un array di numeri indovinati.
+
+4- Mostra i numeri indovinati e il punteggio su H1
+
+ while con lunghezza ArrayRandom < contatore
+- stampa il contatore con iinerHtml( punteggio)
+- br con append
+- stampa i numeri corretti dell'array con innerhtml 
+( se non funziona con lo stampo dei numeri con Array ca)
+*/
